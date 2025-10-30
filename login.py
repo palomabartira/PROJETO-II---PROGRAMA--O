d17c2893987850ba069cda532a1138d2e012146e
Flávia <141580331+flavinharv@users.noreply.github.com>
2025-10-30 13:49:39 -0300

    Atualização de estilização - login

--- login.py
+++ login.py
@@ -9,16 +9,15 @@ class Login():
         """ Inicializa o login, solicitando nome e senha e realizando a autenticação.
         Se as credenciais estiverem corretas, chama a função 'menu'. """
 
-        self.nome = input("Informe seu nome: ").capitalize()
-        self.senha = input("Informe sua senha: ")
+        self.nome = input("👤 Informe seu nome: ").capitalize()
+        self.senha = input("🔒 Informe sua senha: ")
 
         with open('usuarios.json', 'r') as arquivo:
             dados = json.load(arquivo)
 
             for u in dados:
                 if u["Nome"] == self.nome and u["Senha"] == self.senha:
-                    print(f"Bem-vindo(a), {self.nome}!")
+                    print(f"\n 🎉 Bem-vindo(a), {self.nome}!🎉")
                     menu(self.nome)
                     return
-                
-            print("Usuário ou senha incorretas!")
+            print("\n\033[31m❌ Usuário ou senha incorretas!\033[0m")
